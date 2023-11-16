d0972eabdba1ca398f52762b3856f2d3d768a623
Asilvazavala <asilvazavala@gmail.com>
2023-11-16 17:28:54 -0600

    modified menu responsive size icons navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,14 +15,14 @@ export function Header() {
     <section className="container mx-auto my-5 lg:px-8">
       <article className="flex items-center justify-between px-5 md:px-0">
         <Link href="/">
-          <BsBuildings className="text-2xl" />
+          <BsBuildings className="text-2xl w-6 h-6" />
         </Link>
 
         <Navbar openMobileMenu={openMobileMenu} setOpenMobileMenu={setOpenMobileMenu} />
 
         <article className='flex items-center gap-2 md:gap-5'>
           <Link href="tel:1234567890" className='flex items-center gap-4 cursor-pointer'>
-            <BsTelephone />
+            <BsTelephone className="text-gray w-5 h-5" />
             <span className='hidden md:block'>+52 123 456 7890</span>
           </Link>
           <Link href="/" className='mr-1 lg:mr-0 py-1 px-2 lg:px-3 lg:py-2 text-white rounded-lg bg-secondary
