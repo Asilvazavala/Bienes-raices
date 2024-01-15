f65d0870d8fed89e51c55e520f97f7e405347c9e
Asilvazavala <asilvazavala@gmail.com>
2024-01-15 10:23:57 -0600

    modified elements footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export function Footer() {
   return (
     <section className='px-4 py-8 bg-black/90 md:py-40 md:px-36'>
-      <article className='grid gap-8 grid-cols-2 md:grid-cols-[1fr,1fr,1fr,_400px] text-white'>
+      <article className='grid gap-8 text-center grid-cols-1 md:grid-cols-[1fr,1fr,_400px] text-white'>
         {dataFooter.map(({id, links}) => (
           <div key={id}>
             {links.map(({id, name, link}) => (
@@ -19,11 +19,11 @@ export function Footer() {
             ))}
           </div>
         ))}
-        <aside className='md:text-right'>
+        <aside className='lg:text-right'>
           <h4 className='mb-6 text-xl font-semibold'>Bienes raíces</h4>
           <p>Calle Juárez, 25</p>
           <p>Guanajuato, México</p>
-          <div className='flex gap-4 mt-5 md:justify-end'>
+          <div className='flex gap-4 mt-5 lg:justify-end justify-center'>
             <Link href="https://instagram.com" target='_blank' rel='noopener'><LiaInstagram className="text-3xl cursor-pointer transition hover:text-purple-500" /></Link>
             <Link href="https://www.linkedin.com/in/antonio-silva-developer/" target='_blank' rel='noopener'><LiaLinkedinIn className="text-3xl cursor-pointer transition hover:text-blue-500" /></Link>
             <Link href="https://www.pinterest.com.mx/" target='_blank' rel='noopener'><LiaPinterestP className="text-3xl cursor-pointer transition hover:text-red-300" /></Link>
